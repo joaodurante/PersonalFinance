ef0581a84c23ff546a90e399bbdea198c9971e97
João Pedro Durante <jaodurante@gmail.com>
2019-01-06 21:25:09 -0200

    *Created messages file that contains the error messages

--- backend/api/billingCycle/billingCycle.ts
+++ backend/api/billingCycle/billingCycle.ts
@@ -1,21 +1,29 @@
+/**
+ * Schemas para o mongoose, junto com as respostas de possíveis erros
+ */
+
 import * as restful from 'node-restful';
+import { messages } from './messages';
 const mongoose = restful.mongoose;
 
+const reqTrue = [ true, messages.required ];
+
+//mapeamento das informaçoes para o mongoose
 const creditSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    value: { type: Number, min: 0, required: true }
+    name: { type: String, required: reqTrue },
+    value: { type: Number, min: [0, messages.min], required: reqTrue }
 });
 
 const debtSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    value: { type: Number, min: 0, required: true },
-    status: { type: String, required: true, uppercase: true, enum: ['PAID', 'PENDING', 'SCHEDULED'] }
+    name: { type: String, required: reqTrue },
+    value: { type: Number, min: [0, messages.min], required: reqTrue },
+    status: { type: String, required: reqTrue, uppercase: true, enum: ['PAID', 'PENDING', 'SCHEDULED'] }
 });
 
 const billingCycleSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    month: { type: Number, min: 1, max: 12, required: true },
-    year: { type: Number, min: 1970, max: 2100, required: true },
+    name: { type: String, required: reqTrue },
+    month: { type: Number, min: [1, messages.min], max: [12, messages.max], required: reqTrue },
+    year: { type: Number, min: [1970, messages.min], max: [2100, messages.min], required: reqTrue },
     credits: [creditSchema],
     debts: [debtSchema]
 });
--- backend/api/billingCycle/billingCycleService.ts
+++ backend/api/billingCycle/billingCycleService.ts
@@ -1,5 +1,11 @@
 import { BillingCycle } from './billingCycle';
 
+//cria serviços rest para os métodos http
 BillingCycle.methods(['get', 'post', 'put', 'delete']);
+
+//faz com que ao atualizar um dado através de requisições, o mesmo retorne o novo dado e não o antigo
+//faz com que toda alteração passe pela validação dos novos dados (por padrão não passa)
+BillingCycle.updateOptions({new: true, runValidators: true});
+
 
 export { BillingCycle };
