dfafb49f4ec8bfa625698581ed5b243b84fa2dac
João Pedro Durante <jaodurante@gmail.com>
2019-01-17 00:39:44 -0200

    * Improvements at payment cicles view

--- frontend/app/billingCycle/billingCycleController.js
+++ frontend/app/billingCycle/billingCycleController.js
@@ -2,19 +2,41 @@
     angular.module('personalFinance').controller('billingController', [
         '$http',
         'msgs',
+        'tabs',
         BillingCycleController
     ]);
 
-    function BillingCycleController($http, msgs){
+    function BillingCycleController($http, msgs, tabs){
         const vm = this;
-        vm.create = function() {
-            const url = 'http://localhost:3000/api/billingCycles';
-            $http.post(url, vm.billingCycle).then(function(res){
+        const url = 'http://localhost:3000/api/billingCycles';
+
+        vm.refresh = function() {
+            $http.get(url).then((res) => {
                 vm.billingCycle = {};
+                vm.billingCycles = res.data;
+                tabs.show(vm, {tabList: true, tabCreate: true});
+            })
+        }
+
+        vm.create = function() {
+            $http.post(url, vm.billingCycle).then((res) => {
+                vm.refresh();
                 msgs.addSuccess('Successful operation!');
             }).catch(function(res){
                 msgs.addError(res.data.errors);
             })
         }
+
+        vm.showTabUpdate = function(billingCycle){
+            vm.billingCycle = billingCycle;
+            tabs.show(vm, {tabUpdate: true});
+        }
+
+        vm.showTabDelete = function(billingCycle){
+            vm.billingCycle = billingCycle;
+            tabs.show(vm, {tabDelete: true});
+        }
+
+        vm.refresh();
     }
 })();
